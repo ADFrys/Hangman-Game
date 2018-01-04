906ea8314edadac206ca2ddb550894ab079d9cbc
AngelaF <ADFrys@users.noreply.github.com>
2018-01-04 10:59:55 -0600

    Fix game logic.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,27 +14,51 @@ var guessesRemainingp = document.getElementById("guesses-remaining");
 var alreadyGuessedp = document.getElementById("already-guessed");
 var currentwordp = document.getElementById("current-word");
 
-var word = words[Math.floor(Math.random() * words.length)];
-// look in to answerArray = "_" * word.length
 var answerArray = [];
+var word = "";
+var remainingLetters = 0;
+var alreadyGuessedArray = [];
 
-var remainingLetters = word.length;
-
-var alreadyGuessedp = [];
-
-for (var i=0; i<word.length; i++) {
-  answerArray[i] = "_";
+var randomWord = function() {
+  word = words[Math.floor(Math.random() * words.length)];
+  remainingLetters = word.length;
+  answerArray= [];
+  
+  for (var i=0; i<word.length; i++) {
+    answerArray[i] = "_";
   };
+};
+
+randomWord();
 
 currentwordp.textContent = answerArray;
 console.log("answerarray " + answerArray);
-console.log(currentwordp.textContent + " currentwordp1");
 console.log("wordpicked " + word);
 
-  var reset = function () {
-    guessesRemaining = 12;
-    guessesRemainingp.textContent = "Guesses Remaining: " + guessesRemaining;
+var reset = function () {
+  guessesRemaining = 12;
+  guessesRemainingp.textContent = "Guesses Remaining: " + guessesRemaining;
+  alreadyGuessedp.textContent = "Letters Already Guessed: ";
+  randomWord();
+  currentwordp.textContent = answerArray;
+  alreadyGuessedArray = [];
+  console.log("wordpicked reset " + word);
+}
+
+var winLoss = function() {
+  if (word == answerArray.join("")) {
+  	console.log(answerArray);
+    currentwordp = document.getElementById("current-word");
+    currentwordp.textContent = answerArray.join(" "); 
+  	alert("You win!");
+    reset();
   }
+
+  if (guessesRemaining == 0) {
+    alert("You lost. Try again.");
+    reset();
+  }
+}
 
 document.onkeyup = function(event) {
 
@@ -44,37 +68,29 @@ document.onkeyup = function(event) {
   if (possible.includes(userGuess)) {
     guessesRemaining--;
     guessesRemainingp.textContent = "Guesses Remaining: " + guessesRemaining;
-    alreadyGuessedp.textContent = "Already Guessed: " + userGuess;
-    console.log("alreadyguessed " + alreadyGuessedp);
 
-      for (var j=0; j<word.length; j++) {
+    for (var j=0; j<word.length; j++) {
 
-        if (word[j] === userGuess) {
-          console.log("wordj " + word[j]);
-          answerArray[j] = userGuess;
-          currentwordp.textContent = answerArray.join(" ");
-          console.log("answerarrayj " + answerArray[j]);
-          remainingLetters--;
-          console.log("userGuess2 " + userGuess);
-          console.log("remainingletters " + remainingLetters);
-      	  console.log("word " + word);
-        }
-
-        answerArray.toString();
-        console.log("answerArray " + answerArray);
-
-// Even though "word" and "answerArray" variables match, answerArray returns as "d, o, g" for example.
-// this is why the logic does not work. I tried the .toString function but it didn't make a difference. 
-      if (word == answerArray) {
-      	alert("You win!");
-        reset();
+      if (word[j] === userGuess) {
+        console.log("wordj " + word[j]);
+        answerArray[j] = userGuess;
+        currentwordp.textContent = answerArray.join(" ");
+        console.log("answerarrayj " + answerArray[j]);
+        remainingLetters--;
+        console.log("userGuess2 " + userGuess);
+        console.log("remainingletters " + remainingLetters);
+    	  console.log("word " + word);
       }
-      }
-    // }
+
+     	else if ((word[j] !== userGuess) && (alreadyGuessedArray.includes(userGuess) == false)) {
+     		alreadyGuessedArray.push(userGuess);
+     		alreadyGuessedp.textContent += userGuess + " ";
+     	}
+    }
   }
 
   else {
   	alert("You pressed a wrong key! Please select an 'a' through a 'z' key only");
   };
+  winLoss();
 };
-
